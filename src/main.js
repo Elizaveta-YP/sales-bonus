72166e5ac211390c8d8dd5209cb5c9f431d307f1
Elizaveta Chesheyko <elizavetacesejko@gmail.com>
2025-08-23 07:27:18 +0500

    готово

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ function calculateBonusByProfit(index, total, seller) {
     } else {
         bonusPercentage = 0; 
     }
-      return profit;
+      return profit * bonusPercentage;
 }
 
 /**
@@ -204,7 +204,7 @@ sellerStats.forEach((seller, index) => {
    return sellerStats.map(seller => ({
     seller_id: seller.id.toString(),
     name: seller.name,
-    revenue: Math.round(seller.revenue * 100) / 100,
+    revenue: +seller.revenue.toFixed(2),
     profit: +seller.profit.toFixed(2),
     sales_count: seller.sales_count,
     top_products: seller.top_products,
